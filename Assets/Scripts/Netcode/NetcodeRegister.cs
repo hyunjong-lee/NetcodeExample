66f5eacfaeec1b9aee34358cb6f581f0481d52f5
Hyunjong Lee <hyunjong.lee.s@gmail.com>
2023-01-09 01:03:00 +0900

    Before apply one-click register

--- Assets/Scripts/Netcode/NetcodeRegister.cs
+++ Assets/Scripts/Netcode/NetcodeRegister.cs
@@ -1,5 +1,7 @@
 using System.IO;
+using System.Linq;
 using System.Text;
+using UnityEditor;
 using UnityEngine;
 
 public class NetcodeRegister : MonoBehaviour
@@ -20,11 +22,33 @@ public class NetcodeRegister : MonoBehaviour
             pos++;
         }
 
+        var spaces = lines[pos].Substring(0, lines[pos].IndexOf('N'));
+        builder.AppendLine(string.Concat($"{spaces}NetworkPrefabs:"));
         var searchPath = Path.Join(Application.dataPath, RootPath);
         foreach (var prefabPath in Directory.EnumerateFiles(searchPath, "*.prefab", SearchOption.AllDirectories))
         {
-            
+            var assetPath = Path.Join("Assets", prefabPath.Replace(Application.dataPath, ""));
+            var obj = AssetDatabase.LoadAllAssetsAtPath(assetPath).First(e => e.GetType() == typeof(GameObject));
+            if (AssetDatabase.TryGetGUIDAndLocalFileIdentifier(obj, out var guid, out long fileId))
+            {
+                builder.Append(spaces);
+                builder.AppendLine("- Override: 0");
+                builder.Append(spaces);
+                builder.AppendLine("  Prefab: {fileID: @@FILEID@@, guid: @@GUID@@, type: 3}");
+                builder.Append(spaces);
+                builder.AppendLine("  SourcePrefabToOverride: {fileID: 0}");
+                builder.Append(spaces);
+                builder.AppendLine("  SourceHashToOverride: 0");
+                builder.Append(spaces);
+                builder.AppendLine("  OverridingTargetPrefab: {fileID: 0}");
+            }
         }
+
+        Debug.Log(builder);
+
+        while (++pos < lines.Length && lines[pos][spaces.Length] is ' ' or '-') ;
+        while (pos < lines.Length) builder.AppendLine(lines[pos++]);
+
         Debug.Log(builder);
     }
 }
